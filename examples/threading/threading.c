4f6960a0667918eceb0c418501ab4ba8a31ce58d
Thomas Hurtt <tom@mouse.local>
2026-05-26 21:56:45 -0400

    Assignment 4, part 1

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -1,4 +1,5 @@
 #include "threading.h"
+#include <pthread.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -10,24 +11,41 @@
 
 void* threadfunc(void* thread_param)
 {
+    struct thread_data * func_args = (struct thread_data *)thread_param;
 
-    // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
-    // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    usleep(func_args->wait_to_obtain_ms * 1000);
+    int lock_status = pthread_mutex_lock(func_args->mutex);
+    if( lock_status != 0 )
+    {
+        func_args->thread_complete_success = false;
+        return thread_param;
+    }
+
+    usleep(func_args->wait_to_release_ms * 1000);
+    pthread_mutex_unlock(func_args->mutex);
+
+    func_args->thread_complete_success = true;
+
     return thread_param;
 }
 
 
 bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int wait_to_obtain_ms, int wait_to_release_ms)
 {
-    /**
-     * TODO: allocate memory for thread_data, setup mutex and wait arguments, pass thread_data to created thread
-     * using threadfunc() as entry point.
-     *
-     * return true if successful.
-     *
-     * See implementation details in threading.h file comment block
-     */
-    return false;
-}
+    struct thread_data * tdata = (struct thread_data *)malloc(sizeof(struct thread_data));
 
+    tdata->mutex = mutex;
+    tdata->wait_to_obtain_ms = wait_to_obtain_ms;
+    tdata->wait_to_release_ms = wait_to_release_ms;
+
+    int thread_result = pthread_create(thread, NULL, threadfunc, tdata);
+
+    if (thread_result != 0)
+    {
+        ERROR_LOG("Error creating thread: %d", thread_result);
+        free(tdata);
+        return false;
+    }
+
+    return true;
+}
